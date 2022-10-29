4958c9ec3ae542049a4a94c6b31482c4ac8b1814
Alexandr <ssumatoha@gmail.com>
2022-10-29 13:05:12 +0300

    fixed commit

--- src/Garage.java
+++ src/Garage.java
@@ -8,14 +8,27 @@ public class Garage {
     private ArrayList<Lada> ladas = new ArrayList<>();
     private ArrayList<BMW> bmws = new ArrayList<>();
 
+    public Garage(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+    }
+
     public void addCar(Lada lada) {
         this.ladas.add(lada);
+        this.ladaCount += 1;
     }
 
     public void addCar(BMW bmw) {
         this.bmws.add(bmw);
+        this.BMWCount += 1;
     }
 
-
+    public void getCars() {
+        for (Lada i:ladas) {
+            System.out.println("Машина: " + i);
+        }
+        for (BMW i:bmws) {
+            System.out.println("Машина: " + i);
+        }
+    }
 
 }
